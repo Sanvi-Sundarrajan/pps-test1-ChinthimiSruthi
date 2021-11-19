3f74ac48a4bddcfd5162ce8ddbfd984410eac21e
ChinthimiSruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2021-11-19 19:06:02 +0530

    Update p2original.c

---
+++ p2original.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two values\n");
+    scanf("%d %d",&a,&b);
+    int k=a+b;
+    printf("Sum of %d+%d is %d\n",a,b,k);
+    return 0;
+}
