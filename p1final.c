09b8362fc160f0017a07f4ff5ba643ae5f96c178
ChinthimiSruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2021-11-19 18:56:22 +0530

    Update p1final.c

---
+++ p1final.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter the two numbers\n");
+    scanf("%d %d",&i,&j);
+    int k=i+j;
+    printf("The addition of %d and %d is %d\n",i,j,k);
+    return 0;
+}
